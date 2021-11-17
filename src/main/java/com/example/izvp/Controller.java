075bce1477c35bb15d6e54314ab4ce23e1e8bf06
Denis Kud <dekud2109@gmail.com>
2021-11-17 10:54:43 +0200

    Import Sample Controller Skeleton Automatic change from SceneBuilder

--- src/main/java/com/example/izvp/Controller.java
+++ src/main/java/com/example/izvp/Controller.java
@@ -1,14 +1,34 @@
 package com.example.izvp;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button authSigInButton;
+
+    @FXML
+    private Button loginSignUpButton;
+
+    @FXML
+    void initialize() {
+
     }
 }

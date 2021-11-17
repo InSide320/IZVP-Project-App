14ee0365a0deb4258e62c4e04224c9c3e284a643
Denis Kud <dekud2109@gmail.com>
2021-11-17 13:18:18 +0200

    Add new fxml Create new icon in Application Create new class HomeController for Action listener

--- src/main/java/com/example/izvp/Application.java
+++ src/main/java/com/example/izvp/Application.java
@@ -5,14 +5,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.util.Scanner;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("sample.fxml"));
+        FXMLLoader fxmlLoaderApp = new FXMLLoader(Application.class.getResource("app.fxml"));
+        stage.setTitle("My First App");
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
-        stage.setTitle("My First APP");
+        Scene scaneHome = new Scene(fxmlLoaderApp.load(),700,400);
         stage.setScene(scene);
+        stage.setScene(scaneHome);
         stage.show();
     }
 
